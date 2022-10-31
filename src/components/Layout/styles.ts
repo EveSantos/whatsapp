2f3f2a81dfadd6f67f6f2b23b5ea4213587e0151
Evelyn dos Santos <vrevyn@gmail.com>
2022-10-31 13:09:07 -0300

    :sparkles: layout e style do projeto  :sparkles:

---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+// PO PROFLE OPTIONS
+// PC PROFILE CHAT
+// SB SEARCH BAR
+// CH CHAT HISTORY
+// FL LIST FRIENDS 
+// INPUT CHAT
+
+export const Grid = styled.div`
+    display: grid;
+
+    grid-template-columns: ;
+    grid-template-rows: ;
+
+    grid-template-areas: 
+    'PO PC'
+    'SB CH'
+    'FL CH'
+    'FL IC';
+
+    height: 100vh;
+`
